8cbfaa263138bf3f4583837da1c63a651723f47f
Soumaya <soumaya.assadiki@gmail.com>
2021-11-08 19:09:11 +0100

    add a mock server

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,24 @@
-import PropTypes from 'prop-types'
-import { useLocation } from 'react-router-dom'
-import Button from './Button'
+import { useLocation } from "react-router-dom";
+import Button from "./Button";
 
 const Header = ({ title, onAdd, showAdd }) => {
-    const location = useLocation()
-    return (
-        <header className='header'>
-            <h1>{title}</h1>
-            {location.pathname === '/' && (<Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd} />)}
-        </header>
-    )
-}
+  const location = useLocation();
+  return (
+    <header className="header">
+      <h1>{title}</h1>
+      {location.pathname === "/" && <Button color={showAdd ? "red" : "green"} text={showAdd ? "Close" : "Add"} onClick={onAdd} />}
+    </header>
+  );
+};
 
 Header.defaultProps = {
-    title: 'Task Tracker',
-}
+  title: "Task Tracker",
+};
 
 // CSS in JS
 // const headingStyle = {
-//     color: 'red' , 
+//     color: 'red' ,
 //     backgroundColor: 'black'
 // }
 
-export default Header
+export default Header;
